8bd68957ffccdb3b526c9811671e0be580e368c2
Ubuntu <ubuntu@ip-172-31-83-110.ec2.internal>
2019-11-13 22:11:14 +0000

    fixed typos, added flipCard class in css

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,9 @@
 const allCards = document.querySelectorAll(".card"); 
 /*makes list of all card elemets, stores it in const allCards*/
 function flipCard(){
-    this.classlist.toggle("flip")
+    this.classList.toggle("flip");
 }
 /*"this" access the classlist of the "card", and then we toggle the flip class*/
-allCards.forEach(card => card.addEventlistener("click", flipCard))
+allCards.forEach(card => card.addEventListener("click", flipCard));
 /*loops through that list - into each one of the cards, we are going to attach an Eventlistener. When that event is fired, we are going to execute a functoin named flipCard*/
  
