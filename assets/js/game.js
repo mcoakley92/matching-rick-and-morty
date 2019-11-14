d1bed0e451be1e093bffd8697c2b6931a22ba131
Ubuntu <ubuntu@ip-172-31-83-110.ec2.internal>
2019-11-14 21:18:32 +0000

    added condition to flipCard function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,22 @@
 const allCards = document.querySelectorAll(".card"); 
 /*makes list of all card elemets, stores it in const allCards*/
+
+let hasCardFlipped = false;
+let firstClick, secondClick;
+
+
 function flipCard(){
-    this.classList.toggle("flip");
+    this.classList.add("flip"); /*changed toggle to add so we can add a condition*/
+    if(!hasCardFlipped){
+        /*first click*/
+        hasCardFlipped = true;
+        firstClick = this; /*this was checked using console.log(hasCardFlipped, firstClick)*/
+    } else {
+        hasCardFlipped = false;
+        secondClick = this;
+    }
+    
+    
 }
 /*"this" access the classlist of the "card", and then we toggle the flip class*/
 allCards.forEach(card => card.addEventListener("click", flipCard));
